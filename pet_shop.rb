0262e6ea03b265b3acd568568874786120bdb064
wickertongue <haydenwb@gmail.com>
2019-08-03 21:57:16 +0100

    4/28 questions answered

---
+++ pet_shop.rb
@@ -0,0 +1,15 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  return pet_shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
